4583de8af0b8b10057dbffce84e6915f4c16476e
Kirill Zonov <graffzon@gmail.com>
2013-02-02 20:55:12 +0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpreeGemsTest::Application.config.secret_token = 'f239e4397f1c2c627ce813fb0bb8840f769d4814efc223336a2bb70ef9dd28305ba5ae86acd7fc34eb5b2e3c167825331996cd2c2250f8fb982b366f5d7440cd'
